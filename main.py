4aa727ae55cfe0ed4fbb43d9e1fe946b7f6e2cbe
Mohan Instantnoodlep <158505561+mohaninstantnoodlep@users.noreply.github.com>
2024-02-02 03:30:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ api_hash = "a8c50ddacb21496549697fe8dbc86d3e"
 bot_token = "6983357194:AAEGAQSVkx7Nld_ji1yWf5ZQuTASCIQ3bBI"
 
 app = Client(
-    name = "MOHANPRO",
+    name = "MOHAN_MUSIC_PRO",
     api_id = api_id,
     api_hash = api_hash,
     bot_token = bot_token,
